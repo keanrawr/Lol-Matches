8d74025f8a46e0e587be0841cebf5b437f6f332e
Antonio Herrera <the.keanraw@gmail.com>
2020-06-22 11:03:00 -0500

    Save files to aws s3 Added integration to write to aws s3 bucket with the aws python SDK

--- matches-na.py
+++ matches-na.py
@@ -1,5 +1,6 @@
 import os
 import json
+import boto3
 import logging
 from riotwatcher import LolWatcher
 from utils import get_general_stats, get_team_stats, get_participants_stats
@@ -10,13 +11,17 @@ watcher = LolWatcher(api_key)
 region = 'na1'
 match_id = 2868055755
 
+# logging params
 log_path = os.path.join(write_path, 'logs', 'na.log')
 logging.basicConfig(
     filename=log_path,
     level=logging.INFO,
-    format='%(asctime)s %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt="%Y-%m-%d %H:%M:%S"
 )
+
+# aws params
+s3 = boto3.client('s3')
 
 # infinite loop
 while True:
@@ -28,10 +33,11 @@ while True:
 
         match_stats = {**general_stats, **teams_stats, **participant_stats}
 
-        json_path = os.path.join(write_path, 'matches', 'na', f'{match_id}.json')
-        with open(json_path, 'w') as f:
-            json.dump(match_stats, f)
-        logging.info(f'Saving match data for match id:{match_id}')
+        # save to s3
+        json_path = os.path.join('lol', 'matches', 'na', f'{match_id}.json')
+        json_stats = json.dumps(match_stats)
+        s3.put_object(Body=json_stats, Bucket='kean-analytics', Key=json_path)
+        logging.info(f'Saved match data for match id:{match_id}')
     except:
         logging.error(f'Failed to fetch data for match id:{match_id}')
     match_id += 1

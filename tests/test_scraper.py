748660bba55fbf17377ef7f448f8a44b5620088e
Antonio Herrera <the.keanraw@gmail.com>
2021-12-23 12:28:34 -0600

    Moving API call to only be done once

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -2,34 +2,24 @@ from lol_matches.scraper import MatchScraper
 
 region = 'europe'
 match_id = 5053413844
+scraper = MatchScraper(region)
+match = scraper.get_match(match_id)
 
 
 def test_scraper():
-    scraper = MatchScraper(region)
-    match = scraper.get_match(match_id)
-
     assert isinstance(match, dict)
 
 
 def test_match_parser():
-    scraper = MatchScraper(region)
-    match = scraper.get_match(match_id)
     parsed = MatchScraper.parse_match_info(match)
-
     assert isinstance(parsed, dict)
 
 
 def test_team_parser():
-    scraper = MatchScraper(region)
-    match = scraper.get_match(match_id)
     parsed = MatchScraper.parse_teams(match)
-
     assert isinstance(parsed, list)
 
 
 def test_participant_parser():
-    scraper = MatchScraper(region)
-    match = scraper.get_match(match_id)
     parsed = MatchScraper.parse_participants(match)
-
     assert isinstance(parsed, list)

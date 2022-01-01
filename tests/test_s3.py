013c757541295ebae694665aa21fdd110841801d
keanrawr <the.keanraw@gmail.com>
2022-01-01 00:12:06 -0600

    Adding fetch data test cases

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,9 +1,27 @@
 import os
 from lol_matches.s3 import S3Helper
 
-region = 'europe'
 bucket_name = os.getenv('S3_BUCKET_NAME')
-s3 = S3Helper(bucket_name, region)
+s3 = S3Helper(bucket_name)
+
+def test_fetch_data():
+    files = s3.get_treated_dataset('test', 'v0', '.txt')
+    exist = [os.path.isfile(file) for file in files]
+
+    assert all(e for e in exist)
+
+    for file in files:
+        os.remove(file)
+
+
+def test_fetch_data_dir():
+    files = s3.get_treated_dataset('test', 'v0', '.txt', name='data/out/train')
+    exist = [os.path.isfile(file) for file in files]
+
+    assert all(e for e in exist)
+
+    for file in files:
+        os.remove(file)
 
 
 # Test is too expensive if number of saved matches gets large enough
